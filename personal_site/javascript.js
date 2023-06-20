a395a0873147afc8372f1d65d4e1d362af345f08
Annorinoo <annjenelr@gmail.com>
2023-06-20 16:54:13 -0700

    complete rework of the site, added a resource folder

--- personal_site/javascript.js
+++ personal_site/javascript.js
@@ -1,2 +1,18 @@
-// Simple script for a dialogue box
-alert("This is a beta for my future website portfolio.");
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+  
+    // Perform form validation
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+  
+    if (nameInput.value.trim() === "" || emailInput.value.trim() === "") {
+      alert("Please fill in all required fields.");
+      return;
+    }
+  
+    // Form submitted successfully
+    alert("Form submitted!");
+  
+    // Perform further processing or submit the form to a server
+  });
+  
